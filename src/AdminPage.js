d6228b8731f905aa97b60279d5b05fbc08d98ddc
krystalpothilat <krystalpothilat@gmail.com>
2024-07-01 00:21:18 -0700

    base routing

---
+++ src/AdminPage.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function AdminPage() {
+  return (
+    <div>
+      admin
+
+    </div>
+  );
+}
+
+export default AdminPage;
--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './HomePage';
+import AdminPage from './AdminPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<HomePage />} /> 
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </Router>
   );
 }
 
