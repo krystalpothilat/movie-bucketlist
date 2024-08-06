eec7ed5ff08f37bd90a8375af2ff1aa2b47b520a
krystalpothilat <krystalpothilat@gmail.com>
2024-08-06 11:30:58 -0700

    update favicon and tab name

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const cors = require('cors');
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5001;
